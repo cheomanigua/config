4117603dc2fa266649928eae46b185be1722d9c0
Sergio Sanchez <ronero.canario@gmail.com>
2020-11-10 03:07:32 +0100

    added openbsd installation for suckless

---
+++ suckless/openbsd/openbsd_autoinstall_dwm.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# If you are running this script, it is because you ran git clone https://github.com/cheomanigua/config.git
+# and run cd config
+
+doas pkg_add git wget coreutils zathura ranger feh
+
+
+### Install and configure dwm
+
+cd
+mkdir suckless 
+cd suckless
+git clone git://git.suckless.org/dwm
+cd dwm
+cp ../../openbsd/dwm/* .
+sudo make clean install
+
+
+### Install and confiture st
+
+cd ..
+git clone git://git.suckless.org/st
+cd st
+cp ../../openbsd/st/* .
+sudo make clean install
+
+
+### Install and configure dmenu
+
+cd ..
+wget https://dl.suckless.org/tools/dmenu-5.0.tar.gz
+tar -xvf dmenu-5.0.tar.gz
+cd dmenu-5.0
+sudo make clean install
+
+
+### Install and configure dwm-bar
+
+cd ..
+git clone https://github.com/joestandring/dwm-bar.git
+cp -r ../openbsd/dwm-bar/* dwm-bar
+cd
+
+
+### Creating and configuring .xinitrc
+mv openbsd/wallpaper.jpg .
+cat <<EOT >> .xinitrc
+setxkbmap es
+feh --bg-scale wallpaper.jpg &
+openbsd/dwm-bar/dwm_bar.sh &
+exec dwm
+EOT
+
+
+#curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+#echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+#sudo apt update
+#sudo apt install -y brave-browser
+
